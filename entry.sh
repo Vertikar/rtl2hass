0a7a81cee687a992273844ac424a57442bf207bc
achambers16 <achambers16@users.noreply.github.com>
2021-10-31 10:27:48 -0700

    Re-enable discovery script

--- entry.sh
+++ entry.sh
@@ -3,5 +3,5 @@ rtl_433 \
 -F mqtt://$MQTT_HOST:$MQTT_PORT,user=$MQTT_USERNAME,pass=$MQTT_PASSWORD,events=$MQTT_TOPIC/events,states=$MQTT_TOPIC/states,devices=$MQTT_TOPIC[/model][/id][/channel:0] \
 -M time \
 -M protocol \
--M level 
-# | python3 /scripts/rtl_433_mqtt_hass.py
+-M level \
+| python3 /scripts/rtl_433_mqtt_hass.py
